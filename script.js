f8eb3a40bd5e4015386e07637be354d5e769a621
rox-io <rox_prawl@icloud.com>
2022-10-02 16:23:59 -0500

    shows social links on button click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const share = document.querySelectorAll("div.share-icon");
 
-for(let icon of share) {
+for (let icon of share) {
     icon.addEventListener("click", () => {
         socialLinks.classList.toggle("display-social-links");
         authorCard.classList.toggle("toggle-active");
@@ -8,12 +8,12 @@ for(let icon of share) {
 }
 const shareBtn = document.querySelectorAll("div.share-icon");
 const socialLinks = document.querySelector("div.card-footer-b");
-const authorCard = document.querySelector("div.card-footer-a");
+//const authorCard = document.querySelector("div.card-footer-a");
 
-for(let btn of shareBtn) {
+for (let btn of shareBtn) {
     btn.addEventListener("click", () => {
-        socialLinks.classList.toggle("toggle-card-footer-b");
-        authorCard.classList.toggle("toggle-card-footer-a");
+        socialLinks.classList.remove("toggle-card-footer-b");
+        //authorCard.classList.toggle("toggle-card-footer-a");
     })
 }
 
