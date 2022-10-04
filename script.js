78ba64dff8138374848575f517f15051d07acd26
rox-io <rox_prawl@icloud.com>
2022-10-03 22:03:03 -0500

    On click the share button hides or shows element

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-const share = document.querySelectorAll("div.share-icon");
-
-for (let icon of share) {
-    icon.addEventListener("click", () => {
-        socialLinks.classList.toggle("display-social-links");
-        authorCard.classList.toggle("toggle-active");
-    })
-}
 const shareBtn = document.querySelectorAll("div.share-icon");
 const socialLinks = document.querySelector("div.card-footer-b");
-//const authorCard = document.querySelector("div.card-footer-a");
+
+// ON CLICK TOGGLE SHARE CARD
 
 for (let btn of shareBtn) {
     btn.addEventListener("click", () => {
-        socialLinks.classList.remove("toggle-card-footer-b");
-        //authorCard.classList.toggle("toggle-card-footer-a");
+        socialLinks.classList.toggle("toggle-card-footer-b");
     })
 }
-
