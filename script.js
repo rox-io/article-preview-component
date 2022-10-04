d46b04dc23f07082ec26c7ecbec8dd117d873701
rox-io <rox_prawl@icloud.com>
2022-10-04 11:35:36 -0500

    renamed some classes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const shareBtn = document.querySelectorAll("div.share-icon");
-const socialLinks = document.querySelector("div.card-footer-b");
+const socialLinks = document.querySelector("div.share-sec");
 
 // ON CLICK TOGGLE SHARE CARD
 
 for (let btn of shareBtn) {
     btn.addEventListener("click", () => {
-        socialLinks.classList.toggle("toggle-card-footer-b");
+        socialLinks.classList.toggle("toggle-share-sec");
     })
 }
