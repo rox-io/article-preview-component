e3f69e9915f390c0aadb120c37971ccc8e8bfb38
rox-io <rox_prawl@icloud.com>
2022-10-17 18:28:06 -0500

    simplfied share section toggling

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const shareBtn = document.querySelectorAll("div.share-icon");
+const shareBtn = document.querySelector("div.share-icon");
 const socialLinks = document.querySelector("div.share-sec");
+const iconImage = document.querySelector(".share-icon img");
 
 // ON CLICK TOGGLE SHARE CARD
 
-for (let btn of shareBtn) {
-    btn.addEventListener("click", () => {
+    shareBtn.addEventListener("click", () => {
         socialLinks.classList.toggle("toggle-share-sec");
+        shareBtn.classList.toggle("white-share-icon");
+        iconImage.classList.toggle("white-share-icon-color");
     })
-}
+
+
+
